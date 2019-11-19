d13bf5d14d1cffe7cefd80ff0d7a1c9d8d5289d9
Dj Gilcrease <digitalxero@gmail.com>
2019-11-18 16:14:05 -0800

    Update install.sh
    enable https://kubernetes.io/docs/tasks/configure-pod-container/configure-service-account/#service-account-token-volume-projection

--- install.sh
+++ install.sh
@@ -64,6 +64,10 @@ networking:
   serviceSubnet: 10.0.0.0/12
   podSubnet: 10.16.0.0/12
 apiServer:
+  extraArgs:
+    "service-account-issuer": "kubernetes.default.svc"
+    "service-account-signing-key-file": "/etc/kubernetes/pki/sa.key"
+    "service-account-api-audiences": "true"
   certSANs:
   - 127.0.0.1
   - localhost
